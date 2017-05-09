e0b5c3233782ed1d8e629fc722165088d42b7e91
Geeku <ggongpei@qq.com>
2017-05-09 12:55:43 +0800

    webpack configure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by geeku on 09/05/2017.
+ */
+import 'CSS/style.scss';
+import './conf/menu.json';
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const index = path.resolve('./src');
+const distDir = path.resolve('./dist');
+
+module.exports = {
+	entry: index,
+	output: {
+		path: distDir,
+		// publicPath: 'static',
+		filename: 'index.js',
+	},
+	resolve: {
+		alias: {
+			DEMO: path.resolve('./src/demo'),
+			CONF: path.resolve('./src/conf'),
+			LIB: path.resolve('./src/lib'),
+			CSS: path.resolve('./src/css'),
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: ['babel-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$|\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: ['css-loader?minimize', 'sass-loader']
+				})
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve('./index.html'),
+			inject: 'body',
+			hash: true
+		}),
+		new ExtractTextPlugin("style.css"),
+	]
+}
